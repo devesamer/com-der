05dc0db834f4cf3163b0ad43c8acaca52ee27fa8
devesamer <167582766+devesamer@users.noreply.github.com>
2025-04-11 03:24:50 +0300

    Add files via upload

--- main/client.py
+++ main/client.py
@@ -1,10 +1,5 @@
 from telethon import TelegramClient
+
 from main.config import Config
-import asyncio
 
-async def start_bot():
-    bot = TelegramClient('bot', Config.API_ID, Config.API_HASH)
-    await bot.start(bot_token=Config.TOKEN)
-    return bot
-
-bot = asyncio.run(start_bot())
+bot = TelegramClient('bot', Config.API_ID, Config.API_HASH).start(bot_token=Config.TOKEN)
